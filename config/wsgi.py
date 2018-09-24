5d7c43a110d14b83a66f00d7422bc5139470ee89
KG314 <honda1340916@gmail.com>
2018-09-24 16:03:37 +0900

    commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -16,5 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
 
+"""
 from whitenoise.django import DjangoWhiteNoise
 applicaton = DjangoWhiteNoise(application)
+"""
